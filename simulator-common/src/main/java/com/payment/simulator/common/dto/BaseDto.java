df748c9c89593c3a124ab76539806180e7f17c6a
danny.she <shedan82@icloud.com>
2025-03-04 19:54:23 +0800

    optimize gradle

--- simulator-common/src/main/java/com/payment/simulator/common/dto/BaseDto.java
+++ simulator-common/src/main/java/com/payment/simulator/common/dto/BaseDto.java
@@ -1,21 +1,23 @@
 package com.payment.simulator.common.dto;
 
 import java.io.Serializable;
-import java.text.DateFormat;
 import java.util.TimeZone;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BaseDto implements Serializable {
     private static final ObjectMapper MAPPER;
 
     static {
-        final DateFormat iso8601 = new ISO8601DateFormat();
-        iso8601.setTimeZone(TimeZone.getTimeZone("UTC"));
-        MAPPER = new ObjectMapper().registerModule(new Jdk8Module()).setDateFormat(iso8601);
+        MAPPER = new ObjectMapper()
+            .registerModule(new Jdk8Module())
+            .registerModule(new JavaTimeModule())
+            .setTimeZone(TimeZone.getTimeZone("UTC"));
     }
 
     @Override
